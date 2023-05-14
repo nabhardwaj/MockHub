e4777eca86df578f5c8b6ed44cb1ff417626f2ec
nabhardwaj <bhardwaj.ashwani92@gmail.com>
2023-05-14 16:42:53 +0530

    update home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,9 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Mock Hub
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Login</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Register</a>.
     </h3>
   </div>
 </template>
